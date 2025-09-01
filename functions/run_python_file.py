341f80c0154380dec2d83d1b263245578e2f0a68
Alfonso Banzon <alfonsobanzon@gmail.com>
2025-09-01 19:03:51 -0400

    added a run function and tests

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,2 +1,22 @@
+import os
+import subprocess
+
 def run_python_file(working_directory, file_path, args=[]):
-    pass
+    try:
+        file_abspath = os.path.abspath(os.path.join(working_directory, file_path))
+        if not file_abspath.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(file_abspath):
+            return f'Error: File "{file_path}" not found.'
+        if not file_abspath.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file.'
+        run_command = ["python3", file_abspath]
+        result = subprocess.run(["python3", file_abspath] + args, capture_output=True, text=True,  timeout=30)
+        if result.stdout:
+            return f"STDOUT: {result.stdout}"
+        if result.returncode is not 0:
+            return f"STDOUT: {result.stdout} \n Process exited with code {result.returncode}"
+        if not result.stdout:
+            return "No Output Produced"
+    except Exception as e:
+        print(f"Error: {e}")
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def tests():
     print(get_files_info("calculator", "."))
@@ -20,4 +21,13 @@ def tests():
     print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
     print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
     print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+    print("\n")
+    print("=========================================")
+    print("\n")
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
+
 tests()
