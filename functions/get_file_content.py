4a5bed8e05bb768ea87a670198e5d95cfb028a72
Alfonso Banzon <alfonsobanzon@gmail.com>
2025-09-01 17:02:58 -0400

    added writefile function and added tests

---
+++ functions/get_file_content.py
@@ -0,0 +1,20 @@
+import os
+# from config import char_limit
+
+char_limit = 10000
+
+def get_file_content(working_directory, file_path):
+    try:
+        workdir_abspath = os.path.abspath(working_directory)
+        file_abspath = os.path.abspath(os.path.join(working_directory, file_path))
+        if not file_abspath.startswith(workdir_abspath):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(file_path):
+            f'Error: File not found or is not a regular file: "{file_path}"'
+        file_obj = open(file_abspath, "r")
+        file_str = file_obj.read()
+        if len(file_str):
+            return f'{file_str[:char_limit]}[...File "{file_path}" truncated at {char_limit} characters]'
+        return file_str
+    except Exception as e:
+        print(f"Error: {e}")
--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
 
 def tests():
@@ -5,5 +6,10 @@ def tests():
     print(get_files_info("calculator", "pkg"))
     print(get_files_info("calculator", "/bin"))
     print(get_files_info("calculator", "../"))
+
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
+    print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
 tests()
